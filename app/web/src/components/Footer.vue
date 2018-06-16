482951722a25004bf8e458b08ba627aafec5ab4a
Leticiasg <leticiagoncalves2005@gmail.com>
2018-06-16 14:59:28 -0300

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Leticia Goncalves bootcamp
+        Leticia Goncalves bootcamp :)
       </div>
       <div class="col-lg-6">
       </div>
